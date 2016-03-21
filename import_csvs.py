10e03d1149fefd5fbee397d1b823eb12fe4d6423
Simon AM <simon@agius-muscat.net>
2016-03-21 16:51:05 +0100

    Sort files by date properly

--- import_csvs.py
+++ import_csvs.py
@@ -5,6 +5,23 @@ from pysqlite import Pysqlite
 from os import remove as delete_file
 
 
+def sort_files_by_date(file_list):
+    # quick and very dirty natural sort algorithm for timestamps
+    temp_list = [file.replace('.csv', '').split('_') for file in file_list]
+    return_list = []
+    months = [int(month[1]) for month in temp_list]
+    # Do it per month
+    for month in range(min(months), max(months) + 1):  # exclusive range, hence + 1
+        current_month = [stamp for stamp in temp_list if int(stamp[1]) == month]
+        # determine min and max numbers for that month
+        least_day = min([int(stamp[0]) for stamp in current_month])
+        largest_day = max([int(stamp[0]) for stamp in current_month])
+        # rebuild the filenames in order for the returning list
+        for day in range(least_day, largest_day + 1):
+            return_list.append('{}_{}_2016.csv'.format(day, month))
+    return return_list
+
+
 def create_streamer_table(db, streamer, ignore_list):
     if streamer not in ignore_list:
         create_statement = 'CREATE TABLE IF NOT EXISTS `{}` (`id` ' \
@@ -26,9 +43,9 @@ def main():
     existing_tables = [name[0] for name in existing_tables]
     directory = os.getcwd() + '/data/'
     data_files = os.listdir(directory)
-    print(data_files)
     print('Starting import of {} CSVs'.format(len(data_files)))
-    for file_name in sorted(data_files):
+    sorted_files = sort_files_by_date(data_files)
+    for file_name in sorted_files:
         csv_data = []
         print('Importing file: {}'.format(file_name))
         with open(directory + file_name, newline='') as csvfile:
@@ -37,15 +54,18 @@ def main():
                 csv_data.append(row)
         for row in tqdm(csv_data):
             # Create a table in the DB according to the name in the row of data
+            # Add an underscore to table names that start with a number, as sqlite complains about them
+            if row[0][0].isdigit():
+                row[0] = '_' + row[0]
             create_streamer_table(database, row[0], existing_tables)
             # Insert the values into the database
             try:
                 database.dbcur.execute('INSERT INTO {} VALUES (NULL, ?, ?, ?, ?)'.format(row[0]), (row[1], row[2], row[3], row[4]))
             except Exception as e:
                 print(e)
-        print('Commiting data')
+        # Commit the data to the db before moving onto the next set of data
         database.dbcon.commit()
-        print('Deleting file: {}'.format(file_name))
+        print('Deleting file: {}, {}/{}'.format(file_name, sorted_files.index(file_name) + 1, len(data_files)))
         delete_file(directory + file_name)
     print('All done :)')
 
