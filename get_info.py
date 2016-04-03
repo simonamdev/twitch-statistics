d29d9957588cebb1d4ac357809864f615ba47897
Simon AM <simon@agius-muscat.net>
2016-04-03 18:03:57 +0200

    Added stream durations, max and average

--- get_info.py
+++ get_info.py
@@ -22,15 +22,63 @@ def get_streamer_dict(db, streamer):
     streamer_dict['followers'] = followers
     streamer_dict['followers_max'] = followers[-1]
     streamer_dict['times'] = [field[4] for field in data]  # times
+    streamer_dict['durations'] = get_stream_durations(streamer_dict['times'])
+    streamer_dict['durations_max'] = max(streamer_dict['durations'])
+    streamer_dict['durations_average'] = calculate_average(streamer_dict['durations'], return_int=False)
     return streamer_dict
 
 
-def calculate_average(number_list):
+def get_stream_durations(stream_times):
+    # pop one field out to avoid odd numbered lists
+    time_deltas = []
+    for i, field in enumerate(stream_times):
+        # if i == 0 or i % 2 == 0:
+        # current field
+        # timestamp format: '2016-01-25 10:46:18'
+        date_part = field.split(' ')[0].split('-')
+        time_part = field.split(' ')[1].split(':')
+        year, month, day = int(date_part[0]), int(date_part[1]), int(date_part[2])
+        hour, minute, second = int(time_part[0]), int(time_part[1]), int(time_part[2])
+        time1 = datetime.datetime(year=year, month=month, day=day, hour=hour, minute=minute, second=second)
+        # caclulate a delta between the current and the next field
+        if i+1 == len(stream_times):
+            break
+        next_field = stream_times[i+1]
+        date_part = next_field.split(' ')[0].split('-')
+        time_part = next_field.split(' ')[1].split(':')
+        year, month, day = int(date_part[0]), int(date_part[1]), int(date_part[2])
+        hour, minute, second = int(time_part[0]), int(time_part[1]), int(time_part[2])
+        time2 = datetime.datetime(year=year, month=month, day=day, hour=hour, minute=minute, second=second)
+        time_deltas.append([time1, time2])
+    stream_durations = []
+    current_stream_duration = 0
+    for time_one, time_two in time_deltas:
+        delta = time_two - time_one
+        # operate on the assumption that different streams are a minimum of 1 hour away from each other
+        if delta.seconds / (60 * 60) > 1:
+            # print('DIFFERENT STREAM FOUND')
+            stream_durations.append(current_stream_duration)
+            current_stream_duration = 0
+        else:
+            # print('{} {}\n{} {}\n\t{}\t{}s'.format(i, time_one, i, time_two, delta, delta.seconds))
+            current_stream_duration += delta.seconds
+    else:
+        # if the data ends then the last recorded stream will not be recorded, so add the deltas list anyway
+        stream_durations.append(current_stream_duration)
+    # change it from seconds to hours
+    stream_durations = [round(stream / (60 * 60), 2) for stream in stream_durations]
+    return stream_durations
+
+
+def calculate_average(number_list, return_int=True):
     average = 0
     for number in number_list:
         average += number
     average /= len(number_list)
-    return int(average)
+    if return_int:
+        return int(average)
+    else:
+        return round(average, 2)
 
 
 def main():
@@ -69,7 +117,6 @@ def main():
     # assign which tier to sort here. If all, just set all_streamer_data
     # streamers_to_sort = all_streamer_data
     streamers_to_sort = tier_one_streamers
-    """
     print('Unsorted:')
     print('Name : Average Viewers : Max Viewers : Followers')
     for streamer in streamers_to_sort:
@@ -79,79 +126,28 @@ def main():
             streamer['viewers_max'],
             streamer['followers_max']
         ))
-    print()
-    print('Sorted by average viewership:')
+    print('\nSorted by average viewership:')
     for streamer in sorted(streamers_to_sort, key=lambda streamer: streamer['viewers_average'], reverse=True):
         print('{} : {}'.format(streamer['name'], streamer['viewers_average']))
-    print()
-    print('Sorted by peak viewership:')
+    print('\nSorted by peak viewership:')
     for streamer in sorted(streamers_to_sort, key=lambda streamer: streamer['viewers_max'], reverse=True):
         print('{} : {}'.format(streamer['name'], streamer['viewers_max']))
-    print()
-    print('Sorted by followers:')
+    print('\nSorted by followers:')
     for streamer in sorted(streamers_to_sort, key=lambda streamer: streamer['viewers_max'], reverse=True):
         print('{} : {}'.format(streamer['name'], streamer['followers_max']))
-    """
+    print('\nStream durations:')
     for streamer in streamers_to_sort:
-        # pop one field out to avoid odd numbered lists
-        if not len(streamer['times']) % 2 == 0:
-            streamer['times'].pop(0)
-        time_deltas = []
-        for i, field in enumerate(streamer['times']):
-            if i == 0 or i % 2 == 0:
-                # current field
-                # timestamp format: '2016-01-25 10:46:18'
-                date_part = field.split(' ')[0].split('-')
-                time_part = field.split(' ')[1].split(':')
-                year, month, day = int(date_part[0]), int(date_part[1]), int(date_part[2])
-                hour, minute, second = int(time_part[0]), int(time_part[1]), int(time_part[2])
-                time1 = datetime.datetime(year=year, month=month, day=day, hour=hour, minute=minute, second=second)
-                # caclulate a delta between the current and the next field
-                next_field = streamer['times'][i+1]
-                date_part = next_field.split(' ')[0].split('-')
-                time_part = next_field.split(' ')[1].split(':')
-                year, month, day = int(date_part[0]), int(date_part[1]), int(date_part[2])
-                hour, minute, second = int(time_part[0]), int(time_part[1]), int(time_part[2])
-                time2 = datetime.datetime(year=year, month=month, day=day, hour=hour, minute=minute, second=second)
-                difference = time2 - time1
-                time_deltas.append(difference)
-        # consolidate all the deltas
-        stream_durations = []
-        time_total = 0
-        for delta in time_deltas:
-            if delta.seconds < (5 * 60):
-                time_total += delta.seconds
-            else:
-                time_total = round(time_total / (60 * 60), 2)
-                stream_durations.append(time_total)
-                time_total = 0
-        else:
-            # if the streamer has only done one stream then a duration will never be appended
-            if len(stream_durations) == 0:
-                time_total = round(time_total / (60 * 60), 2)
-                stream_durations.append(time_total)
-        durations_string = ''
-        # remove durations less than 5 minutes
-        stream_durations = [duration for duration in stream_durations if duration * 60 > 5.0]
-        for duration in stream_durations:
+        print('{} held {} stream/s of:'.format(streamer['name'], len(streamer['durations'])))
+        durations_string = '\t'
+        for duration in streamer['durations']:
             if duration < 1.0:
                 duration = round(duration * 60, 2)
                 durations_string += '{} minutes '.format(duration)
             else:
-                # if not float(duration).is_integer():
-                #     print(duration - int(duration))
                 durations_string += '{} hours '.format(duration)
-        print('{} durations of {} stream/s:\n{}'.format(streamer['name'], len(stream_durations), durations_string))
+        print(durations_string)
+        print('\tLongest Stream: {} hours Average Stream length: {} hours'.format(streamer['durations_max'], streamer['durations_average']))
 
-    """
-    # take the first and last times as strings to put in the axis label
-    min_time_string = streamer_times[0]
-    max_time_string = streamer_times[-1]
-    # convert the times to epoch seconds
-    # pattern: 2016-01-25 10:46:18
-    # pattern = '%Y-%m-%d %H:%M:%S'
-    # streamer_times = [int(time.mktime(time.strptime(str(stamp), pattern))) for stamp in streamer_times]
-    """
 
 if __name__ == '__main__':
     main()
