2973530085d9c3481731e3ef61278a296341f80c
Simon AM <simon@agius-muscat.net>
2016-04-03 14:57:13 +0200

    Moving data info to another script

---
+++ get_info.py
@@ -0,0 +1,94 @@
+from pysqlite import Pysqlite
+from pprint import pprint
+from tqdm import tqdm
+
+
+def get_streamer_dict(db, streamer):
+    data = db.get_db_data(streamer)
+    streamer_dict = dict()
+    streamer_dict['name'] = streamer
+    streamer_dict['viewers'] = [field[1] for field in data]  # viewers
+    streamer_dict['followers'] = [field[2] for field in data]  # followers
+    streamer_dict['times'] = [field[4] for field in data]  # times
+    return streamer_dict
+
+
+def calculate_average(number_list):
+    average = 0
+    for number in number_list:
+        average += number
+    average /= len(number_list)
+    return int(average)
+
+
+def main():
+    # initialise the DB object
+    database = Pysqlite('twitch_stats', 'PC_stats.db')
+    table_names = database.get_db_data('sqlite_sequence')
+    # get the table names. Ignore it if it is called test
+    table_names = [row[0] for row in table_names if not row[0] == 'test']
+    # initialise list for all the data
+    all_streamer_data = []
+    # list any streamers to ignore
+    streamers_to_ignore = ['legenddolby1986']
+    print('Processing Streamer Data')
+    for streamer in tqdm(table_names):
+        if streamer in streamers_to_ignore:
+            # skip if its on the ignore list
+            continue
+        # get the db data from the table of the same name as the streamer
+        all_streamer_data.append(get_streamer_dict(database, streamer))
+
+    print('Filtering streamer data by tier')
+    tier_one_streamers = [streamer for streamer in all_streamer_data if calculate_average(streamer['viewers']) >= 100]
+    tier_two_streamers = [streamer for streamer in all_streamer_data if 100 > calculate_average(streamer['viewers']) >= 50]
+    tier_three_streamers = [streamer for streamer in all_streamer_data if 50 > calculate_average(streamer['viewers']) >= 15]
+    tier_four_streamers = [streamer for streamer in all_streamer_data if calculate_average(streamer['viewers']) < 15]
+
+    print('Tier One: {} ( >100 )\nTier Two: {} ( 99-50 )\nTier Three: {} ( 50-15 )\nTier Four: {} ( <15 )\nTotal: {}'.format(
+        len(tier_one_streamers),
+        len(tier_two_streamers),
+        len(tier_three_streamers),
+        len(tier_four_streamers),
+        len(table_names)
+    ))
+
+
+    print('Tier One Streamers: (Average >=100 viewers)')
+    print('Index : Name : Peak Viewership : Average Viewership : Followers')
+    for streamer_dict in tier_one_streamers:
+        print('{} : {} : {} : {} : {}'.format(
+            tier_one_streamers.index(streamer_dict),
+            streamer_dict['name'], max(streamer_dict['viewers']),
+            calculate_average(streamer_dict['viewers']),
+            streamer_dict['followers'][-1]))
+    print('Tier Two Streamers: (100 > Average >= 50 viewers)')
+    print('Index : Name : Peak Viewership : Average Viewership : Followers')
+    for streamer_dict in tier_two_streamers:
+        print('{} : {} : {} : {} : {}'.format(
+            tier_two_streamers.index(streamer_dict),
+            streamer_dict['name'], max(streamer_dict['viewers']),
+            calculate_average(streamer_dict['viewers']),
+            streamer_dict['followers'][-1]))
+    print('Tier Three Streamers: (50 > Average >= 15 viewers)')
+    print('Index : Name : Peak Viewership : Average Viewership : Followers')
+    for streamer_dict in tier_three_streamers:
+        print('{} : {} : {} : {} : {}'.format(
+            tier_three_streamers.index(streamer_dict),
+            streamer_dict['name'], max(streamer_dict['viewers']),
+            calculate_average(streamer_dict['viewers']),
+            streamer_dict['followers'][-1]))
+
+
+    """
+    # take the first and last times as strings to put in the axis label
+    min_time_string = streamer_times[0]
+    max_time_string = streamer_times[-1]
+    # convert the times to epoch seconds
+    # pattern: 2016-01-25 10:46:18
+    # pattern = '%Y-%m-%d %H:%M:%S'
+    # streamer_times = [int(time.mktime(time.strptime(str(stamp), pattern))) for stamp in streamer_times]
+    """
+
+if __name__ == '__main__':
+    main()
