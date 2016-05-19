ccc38738352f619c81a06da1c93df3a22eb4aeb4
Simon AM <simon@agius-muscat.net>
2016-05-19 10:22:31 +0200

    Added minify setting

--- website/ocellus.py
+++ website/ocellus.py
@@ -1,12 +1,16 @@
 from flask import Flask, render_template
-
+from minify import minify as minify_css
 app = Flask(__name__)
+
+debug_mode = True
 
 
 @app.route('/')
 def index():
-    return render_template('index.html', app_version='Alpha 0.1')
+    return render_template('index.html', app_version='Alpha 0.1', debug_mode=debug_mode)
 
 if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=9000, debug=True)
-    app.run(host='0.0.0.0', port=9000, debug=False)
+    if not debug_mode:
+        minify_css()
+    app.run(host='127.0.0.1', port=9000, debug=debug_mode)
+    # app.run(host='0.0.0.0', port=9000, debug=debug_mode)
