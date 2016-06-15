e518ca3e7d063f2255261425ee976daf899fd517
Simon AM <simon@agius-muscat.net>
2016-06-15 21:11:08 +0200

    Pass the url as a parameter instead

--- twitchapi/twitchapi.py
+++ twitchapi/twitchapi.py
@@ -1,24 +1,41 @@
 import requests
 
 
-class APIRequest:
-    def __init__(self, game_url_name, timeout=10):
+class APIStreamsRequest:
+    def __init__(self, game_url_name, timeout=10, verbose=False):
         self.game_url_name = game_url_name
         self.json_url = 'https://api.twitch.tv/kraken/streams'
         self.timeout = timeout
+        self.status_code = 0
+        self.streams_data = None
+        self.verbose = verbose
 
-    def get_game_data(self):
+    def print(self, string=''):
+        if self.verbose:
+            print(string)
+
+    def get_game_data(self, url=''):
+        if url == '':
+            self.print('[ERROR] No URL passed!')
+            return None
         # url = self.json_url + '?game=' + self.game_url_name
+        self.print('[INFO] Sending a request to: {}'.format(url))
         try:
-            game_data = requests.get(url=self.json_url, params={'game': self.game_url_name})
-            print(game_data)
+            response = requests.get(url=url)
         except Exception as e:
             print(e)
+            return None
+        self.status_code = response.status_code
+        self.streams_data = response.json()['streams']
+
+    def get_streams_data(self):
+        return self.streams_data
 
 
 def main():
-    a = APIRequest(game_url_name='Elite:%20Dangerous')
+    a = APIStreamsRequest(game_url_name='Elite:%20Dangerous')
     a.get_game_data()
+    print(a.get_streams_data())
 
 if __name__ == '__main__':
     main()
