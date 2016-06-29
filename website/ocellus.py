36ed4d12c66d959acacb32d9c3deea77c6e7589e
Simon AM <simon@agius-muscat.net>
2016-06-29 07:58:21 +0200

    Fixed data field names

--- website/ocellus.py
+++ website/ocellus.py
@@ -197,10 +197,10 @@ def api_streamer_data(streamer_name, game_short_name):
     follower_count_dicts = streamer_global_db.get_follower_counts(game_short_name=game_short_name)
     streamer_graph_data = json.dumps({
         'viewers_average': [
-            {'x': row['start_time'], 'y': row['viewers_average']} for row in average_viewer_dicts
+            {'time': row['start_time'], 'value': row['viewers_average']} for row in average_viewer_dicts
         ],
         'followers': [
-            {'x': row['update_time'], 'y': row['followers']} for row in follower_count_dicts
+            {'time': row['update_time'], 'value': row['followers']} for row in follower_count_dicts
         ]
     })
     log_page_visit('streamer_api', '{}_{}'.format(streamer_name, game_short_name), start_time=access_time)
