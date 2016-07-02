23d49d5ad3f16e8e3be94937636910f1f94e1ff6
Simon AM <simon@agius-muscat.net>
2016-07-02 12:28:23 +0200

    Plot using chart data

--- website/static/test_gc.js
+++ website/static/test_gc.js
@@ -1,25 +1,29 @@
+function convertToDate(timeStamp) {
+		var ts = String(timeStamp);
+		// Format: YYYY-MM-DD HH:MM:SS
+		var string_split = ts.split(" ");
+		var date_part = string_split[0].split("-");
+		var time_part = string_split[1].split(":");
+		return new Date(date_part[0], date_part[1], date_part[2], time_part[0], time_part[1], time_part[2]);
+}
+
 function drawLine(chartJSONData) {
-		google.charts.load('current', {packages: ['corechart', 'line'], 'callback': function() {
-
+		console.log("Drawing a chart with data:")
 		console.log(chartJSONData);
-		var data = new google.visualization.DataTable();
-		data.addColumn('number', 'X');
-		data.addColumn('number', 'Dogs');
 
-		data.addRows([
-			  [0, 0],   [1, 10],  [2, 23],  [3, 17],  [4, 18],  [5, 9],
-			  [6, 11],  [7, 27],  [8, 33],  [9, 40],  [10, 32], [11, 35],
-			  [12, 30], [13, 40], [14, 42], [15, 47], [16, 44], [17, 48],
-			  [18, 52], [19, 54], [20, 42], [21, 55], [22, 56], [23, 57],
-			  [24, 60], [25, 50], [26, 52], [27, 51], [28, 49], [29, 53],
-			  [30, 55], [31, 60], [32, 61], [33, 59], [34, 62], [35, 65],
-			  [36, 62], [37, 58], [38, 55], [39, 61], [40, 64], [41, 65],
-			  [42, 63], [43, 66], [44, 67], [45, 69], [46, 69], [47, 70],
-			  [48, 72], [49, 68], [50, 66], [51, 65], [52, 67], [53, 70],
-			  [54, 71], [55, 72], [56, 73], [57, 75], [58, 70], [59, 68],
-			  [60, 64], [61, 60], [62, 65], [63, 67], [64, 68], [65, 69],
-			  [66, 70], [67, 72], [68, 75], [69, 80]
-		]);
+		google.charts.load('current', {packages: ['corechart', 'line'], 'callback': function() {
+		var data = new google.visualization.DataTable();
+
+		convertedData = [];
+		$.each(chartJSONData, function( index, value ) {
+				convertedData.push([convertToDate(value[0]), value[1]]);
+		});
+		console.log(convertedData);
+
+		data.addColumn('date', 'timestamps');
+		data.addColumn('number', 'viewers');
+		data.addRows(convertedData);
+		data.sort([{column: 1}]);
 
 		var options = {
 			  hAxis: {
@@ -43,7 +47,7 @@ function drawCharts(streamerName) {
         url: "/api/v1/streamer/" + streamerName,
         success: function(receivedData) {
             jsonData = JSON.parse(receivedData);
-            drawLine(jsonData['ED']);
+            drawLine(jsonData['ED']['viewers_average']);
 			  }
     });
 }
