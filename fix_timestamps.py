6ab13e9805b1c4544f755bed1ae2479ec28a23da
Simon AM <simon@agius-muscat.net>
2016-07-02 09:16:15 +0200

    Removed vacuums from fix script

--- fix_timestamps.py
+++ fix_timestamps.py
@@ -4,7 +4,7 @@ from neopysqlite.neopysqlite import Pysqlite
 from tqdm import tqdm
 
 
-fix_stream_tables = True
+fix_stream_tables = False
 fix_game_tables = True
 
 games = ['ED',  'PC']
@@ -24,7 +24,7 @@ for game in games:
             for table_name in table_names:
                 rows = db.get_all_rows(table=table_name)
                 for row in tqdm(rows):
-                    # convert anything in DD_MM_YYYY HH:MM:SS to YYYY_MM_DD HH:MM:SS
+                    # convert anything in DD-MM-YYYY HH:MM:SS to YYYY-MM-DD HH:MM:SS
                     old_timestamp = row[1]
                     split_string = old_timestamp.split(' ')
                     date_part = split_string[0].split('-')
@@ -38,8 +38,6 @@ for game in games:
                         db.dbcur.execute('UPDATE {} SET timestamp = ? WHERE timestamp = ?'.format(table_name),
                                          (new_timestamp, old_timestamp))
                 db.dbcon.commit()
-            db.execute_sql('VACUUM')
-            db.dbcon.commit()
     if fix_game_tables:
         game_db_path = os.path.join(os.getcwd(), 'data', game, '{}_data.db'.format(game))
         db = Pysqlite(database_name='{} DB'.format(game), database_file=game_db_path)
@@ -77,5 +75,4 @@ for game in games:
                 db.dbcur.execute('UPDATE streamers_data SET last_updated = ? WHERE last_updated = ?',
                                  (new_timestamp, old_timestamp))
         db.dbcon.commit()
-        db.execute_sql('VACUUM')
-        db.dbcon.commit()
+
