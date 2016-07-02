d41cd6285f8fdb2e98a6958310fec55fb3e1d894
Simon AM <simon@agius-muscat.net>
2016-07-02 12:51:20 +0200

    Generalise drawLine function

--- website/static/test_gc.js
+++ website/static/test_gc.js
@@ -7,38 +7,45 @@ function convertToDate(timeStamp) {
 		return new Date(date_part[0], date_part[1], date_part[2], time_part[0], time_part[1], time_part[2]);
 }
 
-function drawLine(chartJSONData) {
+function drawLine(chartJSONData, gameShortName, valueString) {
 		console.log("Drawing a chart with data:")
 		console.log(chartJSONData);
 
 		google.charts.load('current', {packages: ['corechart', 'line'], 'callback': function() {
-		var data = new google.visualization.DataTable();
+				var data = new google.visualization.DataTable();
 
-		convertedData = [];
-		$.each(chartJSONData, function( index, value ) {
-				convertedData.push([convertToDate(value[0]), value[1]]);
-		});
-		console.log(convertedData);
+				convertedData = [];
+				$.each(chartJSONData, function( index, value ) {
+						convertedData.push([convertToDate(value[0]), value[1]]);
+				});
+				console.log(convertedData);
 
-		data.addColumn('date', 'timestamps');
-		data.addColumn('number', 'viewers');
-		data.addRows(convertedData);
-		data.sort([{column: 0}, {column: 1}]);
+				data.addColumn('date', 'timestamps');
+				data.addColumn('number', 'viewers');
+				data.addRows(convertedData);
+				data.sort([{column: 0}, {column: 1}]);
 
-		var options = {
-			  hAxis: {
-			     title: 'Stream Times'
-			  },
-			  vAxis: {
-			      title: 'Average Viewers'
-			  }
-		};
+				chartContainerWidth = $('#' + gameShortName + '-average-viewers-graph').width();
 
-		var chart = new google.visualization.LineChart(document.getElementById('ED-average-viewers-graph'));
+				var options = {
+						title: valueString + ' over time',
+						width: chartContainerWidth - 20,
+						height: 400,
+		        legend: {
+		            position: 'bottom'
+		        },
+					  hAxis: {
+					     title: 'Stream Times'
+					  },
+					  vAxis: {
+					      title: valueString
+					  }
+				};
 
-		chart.draw(data, options);
-		}
-		});
+				var chart = new google.visualization.LineChart(document.getElementById(gameShortName + '-average-viewers-graph'));
+
+				chart.draw(data, options);
+		}});
 }
 
 function drawCharts(streamerName) {
@@ -47,7 +54,7 @@ function drawCharts(streamerName) {
         url: "/api/v1/streamer/" + streamerName,
         success: function(receivedData) {
             jsonData = JSON.parse(receivedData);
-            drawLine(jsonData['ED']['viewers_average']);
+            drawLine(jsonData['ED']['viewers_average'], 'ED', 'Average Viewers');
 			  }
     });
 }
