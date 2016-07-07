67868b2c4228ed80f3cb0691a90c0604e571acc0
Simon AM <simon@agius-muscat.net>
2016-07-07 22:34:33 +0200

    Added newline to log handler

--- scrape_twitch.py
+++ scrape_twitch.py
@@ -107,7 +107,7 @@ def get_current_date_string():
 
 def log_downtime():
     handler = FileHandler(downtime_log_path, mode='a')
-    handler.emit('downtime|{}'.format(time.time()))
+    handler.emit('downtime|{}\n'.format(time.time()))
     handler.close()
 
 
