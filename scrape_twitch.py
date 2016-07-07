282f4637912c6452b23c6a371fcec153730d4046
Simon AM <simon@agius-muscat.net>
2016-07-07 22:24:02 +0200

    Return data and write to a CSV file

--- scrape_twitch.py
+++ scrape_twitch.py
@@ -102,41 +102,6 @@ def get_current_date_string():
     return '{}_{}_{}'.format(previous_day, previous_month, previous_year)
 
 
-def process_streamer_data(data_list=None):
-    if data_list is None:
-        return []
-    return_data = []
-    for i, streamer_data in enumerate(data_list):
-        streamer_name = streamer_data['channel']['name']
-        # get the data for this streamer
-        viewer_count = streamer_data['viewers']
-        if viewer_count == 0:
-            # skip this streamer if they have no viewers
-            continue
-        follower_count = streamer_data['channel']['followers']
-        partnership = 0
-        if streamer_data['channel']['partner']:
-            partnership = 1
-        print('[{}] {}\t\t({}, {}, {})'.format(
-            i,
-            streamer_name,
-            viewer_count,
-            follower_count,
-            partnership
-        ))
-        # sleep(0.1)  # allows reading the names when checking top streamer
-        # replace the underscores for dashes in this timestamp
-        timestamp = '{} {}:{}:{}'.format(
-                get_current_date_string().replace('_', '-'),
-                datetime.now().hour,
-                datetime.now().minute,
-                datetime.now().second
-        )
-        # add the data to the list
-        return_data.append([streamer_name, viewer_count, follower_count, partnership, timestamp])
-    return return_data
-
-
 def log_downtime():
     handler = FileHandler(downtime_log_path, mode='a')
     handler.emit('downtime|{}'.format(time.time()))
@@ -174,54 +139,13 @@ def main():
             # if the last request was not successful, log to the error log
             if not api.last_request_successful():
                 log_downtime()
-            print(api.return_required_data())
-            """
-            json_url_streams = r'https://api.twitch.tv/kraken/streams?game={}'.format(game['url_name'])
-            # initial api ping to get the first set of streamers
-            try:
-                data_games = requests.get(json_url_streams, timeout=10).json()
-                total_stream_count = data_games['_total']
-            except Exception as e:
-                print('[-] Error getting JSON data for streamer list: {}'.format(e))
-                pause(10)  # delay before trying again
-            else:
-                # write the timestamp to a text file to allow checking for uptime
-                write_tick_timestamp()
-                print('[+] {} ongoing streams for {}'.format(total_stream_count, game['name']))
-                api_offset_count = 0
-                try:
-                    streams_data = data_games['streams']
-                except Exception as e:
-                    print('[-] Key error: {}'.format(e))
-                    continue
-                while not len(streams_data) == 0:
-                    print('[+] Stream count: {} Offset: {}'.format(len(data_games['streams']), api_offset_count))
-                    # only ping the api again if you are not on the first page
-                    if not api_offset_count == 0:
-                        # print('[+] Accessing url: {}'.format(next_json_url))
-                        try:
-                            next_json_url = data_games['_links']['next']
-                            data_games = requests.get(next_json_url, timeout=10).json()
-                        except Exception as e:
-                            print('[-] Error getting JSON data for streamer list: {}'.format(e))
-                    try:
-                        streams_data = data_games['streams']
-                    except Exception as e:
-                        print('[-] Error parsing JSON data: {}'.format(e))
-                        streams_data = []
-                        continue
-                    # only consider the streamer if they are playing the game
-                    game_streamers_data = [row for row in streams_data if row['game'] in game['game_names']]
-                    # store all current stream data in a list
-                    current_stream_data = process_streamer_data(data_list=game_streamers_data)
-                    # Write only if rows have been added
-                    if len(current_stream_data) > 0:
-                        insert_data_rows_into_csv(
-                            file_name=game['shorthand_name'] + '_' + current_date_string,
-                            data_rows=current_stream_data,
-                            verbose=True)
-                    api_offset_count += 90
-                            """
+            returned_data = api.return_required_data()
+            if len(returned_data) > 0:
+                file_name = game_name['short'] + '_' + current_date_string
+                insert_data_rows_into_csv(
+                    file_name=file_name,
+                    data_rows=returned_data,
+                    verbose=True)
         pause(cycle_delay)
 
 if __name__ == '__main__':
